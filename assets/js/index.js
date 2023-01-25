d3984bbe5c7c03e67461237b641e05c63aaa635a
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-01-25 13:35:59 -0300

    Estudando o formulário

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,19 +13,88 @@ class ValidaFormulario{
     handleSubmit(e){
         e.preventDefault();
         const camposValidos = this.camposSaoValidos();
+        const senhasValidas = this.senhasSaoValidas();
+
+        if(camposValidos && senhasValidas){
+            alert('Formulário enviado');
+            this.formulario.submit();
+        }
+    }
+
+    senhasSaoValidas(){
+        let valid = true
+
+        const senha = this.formulario.querySelector('.senha');
+        const repetirSenha = this.formulario.querySelector('.repetir-senha');
+
+        if(senha.value !== repetirSenha.value){
+            valid = false;
+            this.criaErro(senha, 'Campos senha e repetir senha precisam ser iguais');
+            this.criaErro(repetirSenha, 'Campos senha e repetir senha precisam ser iguais');
+        }
+
+        if(senha.value.length < 6 || senha.value.length > 12){
+            valid = false;
+            this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres');
+        }
+
+        return valid;
     }
 
     camposSaoValidos(){
         let valid = true;
 
+        for(let removerErros of this.formulario.querySelectorAll('.error-text')){
+            removerErros.remove();
+        }
+
         for(let campo of this.formulario.querySelectorAll('.validar')) {
-            // const p = campo.previousElementSibling.innerText;
+            const p = campo.previousElementSibling.innerText;
             
             if(!campo.value){
-                this.criaErro(campo, 'Campo tal não pode estar em branco');
+                this.criaErro(campo, `O campo "${p}" precisa estar preenchido` );
                 valid = false;
             }
+
+            if(campo.classList.contains('cpf')){
+                if(!this.validaCPF(campo)) valid = false;
+            }
+
+            if(campo.classList.contains('usuario')){
+                if(!this.validaUsuario(campo)) valid = false;
+            }
         }
+
+        return valid;
+    }
+
+    validaUsuario(campo){
+        const usuario = campo.value;
+        let valid = true
+
+        if(usuario.length < 3 || usuario.length > 12){
+            this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres');
+            valid = false;
+        }
+
+        if(!usuario.match(/^[a-zA-Z0-9]+$/g)){
+            this.criaErro(campo, 'Usuário precisa conter apenas letras e números')
+            valid = false;
+        }
+
+        return valid;
+    }
+
+
+    validaCPF(campo){
+        const cpf = new ValidaCPF(campo.value);
+
+        if(!cpf.valida()) {
+            this.criaErro(campo, 'CPF inválido');
+            return false
+        }
+
+        return true
     }
 
     criaErro(campo, msg){
@@ -37,3 +106,7 @@ class ValidaFormulario{
 }
 
 const valida = new ValidaFormulario();
+
+// Campo -> é o input. 
+// p -> pega o irmão anterior do campo
+// Na linha 34, chama-se a função que retorna um resultado. Se o resultado for false, ou seja, o cpf seja inválido, com o operador de negação o if fica true e assim o que está dentro do if é feito.
