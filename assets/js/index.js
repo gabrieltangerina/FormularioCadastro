f8adc3eb43b7adcbaf25d7619f252fefbcedddcf
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-01-26 13:58:59 -0300

    Refazendo para estudar

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,6 +14,26 @@ class ValidaFormulario{
         e.preventDefault();
 
         const camposValidos = this.camposSaoValidos();
+        const senhasValidas = this.senhasSaoValidas();
+    }
+
+    senhasSaoValidas(){
+        let valid = true;
+
+        const senha = this.formulario.querySelector('.senha');
+        const repetirSenha = this.formulario.querySelector('.repetir-senha');
+
+        if(senha.value !== repetirSenha.value){
+            this.criaErro(senha, 'O campo senha e repetir senha devem ser iguais');
+            this.criaErro(repetirSenha, 'O campo senha e repetir senha devem ser iguais');
+
+            valid = false;
+        }
+
+        if(senha.value.length < 6 || senha.value.length > 12){
+            this.criaErro(senha, 'Senha precisa ter entre 6 e 12 caracteres');
+            valid = false;
+        }
     }
 
     camposSaoValidos(){
@@ -34,13 +54,34 @@ class ValidaFormulario{
             if(campo.classList.contains('cpf')){
                 if(!this.validaCPF(campo)) valid = false;
             }
+
+            if(campo.classList.contains('usuario')){
+                if(!this.validaUsuario(campo)) valid = false;
+            }
+        }
+
+        return valid;
+    }
+
+    validaUsuario(campo){
+        const usuario = campo.value;
+        let valid = true;
+
+        if(usuario.length < 3 || usuario.length > 12){
+            this.criaErro(campo, 'Usuário deverá ter entre 3 e 12 caracteres');
+            valid = false;
+        }
+
+        if(!usuario.match(/^[a-zA-Z0-9]+$/g)){
+            this.criaErro(campo, 'Usuário só poderá conter letrar e/ou números');
+            valid = false;
         }
 
         return valid;
     }
 
     validaCPF(campo){
-        const cpf = ValidaCPF(campo.value);
+        const cpf = new ValidaCPF(campo.value);
 
         if(!cpf.valida()){
             this.criaErro(campo, 'CPF inválido');
